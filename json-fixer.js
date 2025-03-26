cc512f593a8646d1f9fff191b6376f3453601ae8
Matheus H. Potrich <matheushp.com@hotmail.com>
2025-03-26 18:55:23 -0300

    fixed not returning valid json

--- json-fixer.js
+++ json-fixer.js
@@ -6,20 +6,41 @@ function fixJson(wrongJson){
 	wrongJson = wrongJson.replace(/\s/g, "");
 
 	const objects = wrongJson.match(regexObjectValue);
-	const strings = wrongJson.match(regexStringValue);
 
 	if(objects !== null){
 		objects.forEach(object => {
+			wrongJson = wrongJson.replace(object, "");
 			object = `{${object}}`;
 			fixedJson = Object.assign(fixedJson, JSON.parse(object));
 		});
 	}
+
+	const strings = wrongJson.match(regexStringValue);
+
 	
 	if(strings !== null){
 		strings.forEach(string => {
+			wrongJson = wrongJson.replace(string, "");
 			string = `{${string}}`;
+			string = fixJson(string);
 			fixedJson = Object.assign(fixedJson, JSON.parse(string));
 		});
+	}
+	
+	wrongJson = wrongJson.replace("{", "");
+	wrongJson = wrongJson.replace("}", "");
+	wrongJson = wrongJson.replace(",", "");
+
+	const detectOpenPropName = wrongJson.match(/"/g);
+
+	if(detectOpenPropName && detectOpenPropName.lenght == 1){
+		wrongJson = wrongJson + '"';
+	}
+	
+	const detectWithOutValue = wrongJson.match(/:/g);
+
+	if(detectWithOutValue === null){
+		wrongJson = wrongJson + ':"VALUE"';
 	}
 
 	return JSON.stringify(fixedJson);
