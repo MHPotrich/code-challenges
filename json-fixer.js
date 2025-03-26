2e33c5d4ebdffb632278de0ecc596f613540101c
Matheus H. Potrich <matheushp.com@hotmail.com>
2025-03-26 19:43:16 -0300

    fix not auto compliting json

--- json-fixer.js
+++ json-fixer.js
@@ -1,3 +1,27 @@
+function fixIncompleteJson(incompleteJson){
+	const detectOpenPropName = incompleteJson.match(/"\w+[:|}|{]?/g);
+	const detectWithOutValue = incompleteJson.match(/:/g);
+
+	if(detectOpenPropName !== null){
+		detectOpenPropName.forEach(openedName => {
+			const start = incompleteJson.indexOf(openedName);  
+			const end = start + openedName.length;
+			
+			if(end == incompleteJson.length){
+				incompleteJson = incompleteJson + '"';
+			} else {
+				incompleteJson = incompleteJson.slice(0, end) + '"' + incompleteJson.slice(end + 1, incompleteJson.length);
+			}
+		});
+	}
+	
+	if(detectWithOutValue === null){
+		incompleteJson = incompleteJson + ':"VALUE"';
+	}
+
+	return JSON.parse(`{${incompleteJson}}`);
+}
+
 function fixJson(wrongJson){
 	let fixedJson = new Object();
 	const regexStringValue = /"\w+":"\w+"/g;
@@ -30,20 +54,10 @@ function fixJson(wrongJson){
 	wrongJson = wrongJson.replace("{", "");
 	wrongJson = wrongJson.replace("}", "");
 	wrongJson = wrongJson.replace(",", "");
-
-	const detectOpenPropName = wrongJson.match(/"/g);
-
-	if(detectOpenPropName && detectOpenPropName.lenght == 1){
-		wrongJson = wrongJson + '"';
-	}
 	
-	const detectWithOutValue = wrongJson.match(/:/g);
+	if(wrongJson.length === 0) return JSON.stringify(fixedJson);
 
-	if(detectWithOutValue === null){
-		wrongJson = wrongJson + ':"VALUE"';
-	}
-
-	return JSON.stringify(fixedJson);
+	return JSON.stringify(Object.assign(fixedJson, fixIncompleteJson(wrongJson)));
 }
 
 console.log("Test 1:", fixJson('{"ndh": { "sdjfh": "dkfj", "dkjfsh": "dfjkshj" }, "ddl'));
